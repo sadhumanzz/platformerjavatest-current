8c3c2ec2faba9b8ecff04cac58848e7c71210ad0
Clovis <118827018+sadhumanzz@users.noreply.github.com>
2024-04-11 10:01:26 -0400

    more inventory testing

--- inventoryMenu.ts
+++ inventoryMenu.ts
@@ -1,8 +1,10 @@
 //basic inits
 let inventoryVisible: boolean = false
+let holdingOrgan: boolean = false
 
 namespace SpriteKind {
     export const MenuItem = SpriteKind.create()
+    export const Organ = SpriteKind.create()
 }
 
 sprites.onCreated(SpriteKind.MenuItem, function(sprite: Sprite) {
@@ -10,12 +12,54 @@ sprites.onCreated(SpriteKind.MenuItem, function(sprite: Sprite) {
     sprite.setFlag(SpriteFlag.Invisible, true)
 })
 
+sprites.onCreated(SpriteKind.Organ, function (sprite: Sprite) {
+    sprite.setFlag(SpriteFlag.RelativeToCamera, true)
+    sprite.setFlag(SpriteFlag.Invisible, true)
+    sprite.z = 98
+})
+
 let menuBackground = sprites.create(assets.image`menuBackground`, SpriteKind.MenuItem)
 let cursorSprite = sprites.create(assets.image`cursorSprite`, SpriteKind.MenuItem)
 
-menuBackground.z = 99
+menuBackground.z = 97
 cursorSprite.z = 99
 
+let head = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+    . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+    . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+    . . 1 f 1 1 1 1 1 1 1 1 f 1 . .
+    . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+    . . 1 f 1 1 1 1 1 1 1 1 f 1 . .
+    . . 1 f 1 1 1 1 1 1 1 1 f 1 . .
+    . . 1 f 1 1 1 1 1 1 1 1 f 1 . .
+    . . 1 f 1 1 1 1 1 1 1 1 f 1 . .
+    . . 1 f 1 1 1 1 1 1 1 1 f 1 . .
+    . . 1 f f f f f f f f f f 1 . .
+    . . 1 1 1 1 1 1 1 1 1 1 1 1 . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`, SpriteKind.Organ)
+let body = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 .
+    . . . . . . . . . . . . . . . .
+`, SpriteKind.Organ)
 
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
 
@@ -30,6 +74,14 @@ controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
 function openInventory() {
     inventoryVisible = true
     menuBackground.setFlag(SpriteFlag.Invisible, false)
+    head.setFlag(SpriteFlag.Invisible, false)
+    body.setFlag(SpriteFlag.Invisible, false)
+
+    grid.place(cursorSprite, tiles.getTileLocation(1, 2))
+    grid.place(head, tiles.getTileLocation(6, 2))
+    grid.place(body, tiles.getTileLocation(6, 3))
+
+
     cursorSprite.setFlag(SpriteFlag.Invisible, false)
 
     playerImmobile = true
@@ -40,7 +92,10 @@ function closeInventory() {
     inventoryVisible = false
     menuBackground.setFlag(SpriteFlag.Invisible, true)
     cursorSprite.setFlag(SpriteFlag.Invisible, true)
+    head.setFlag(SpriteFlag.Invisible, true)
+    body.setFlag(SpriteFlag.Invisible, true)
     playerImmobile = false
     grid.moveWithButtons(null)
 
 }
+
